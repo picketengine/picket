f35a513523cd27fee0bdddaf7a4de848cd3d3f5d
Tony Burns <tony@tonyburns.net>
2024-11-11 20:36:45 -0500

    chore: release v0.1.0.alpha (#2)

--- lib/picket/version.rb
+++ lib/picket/version.rb
@@ -6,17 +6,17 @@ module Picket
     MAJOR = 0
     MINOR = 1
     TINY  = 0
-    PRE   = nil
+    PRE   = "alpha"
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join(".")
   end
 
-  # Returns the currently loaded version of \Picket as a +Gem::Version+.
+  # Returns the currently loaded version of Picket as a +Gem::Version+.
   def self.gem_version
     Gem::Version.new VERSION::STRING
   end
 
-  # Returns the currently loaded version of \Picket as a string.
+  # Returns the currently loaded version of Picket as a string.
   def self.version
     gem_version.to_s
   end
